3090d299f4b5fec2774f5ddf4e79eccbf74abffc
Andre Ambrosio Boechat <boechat107@tutanota.com>
2022-10-03 18:53:14 -0300

    Add the basic structure to read files

--- initial-utilities/wzip-wunzip/wunzip.c
+++ initial-utilities/wzip-wunzip/wunzip.c
@@ -1,6 +1,25 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[argc+1]) {
+void decode_print_RLE(FILE *stream) {
 
+}
+
+int main(int argc, char *argv[argc+1]) {
+    if (argc < 2) {
+        printf("unwzip: file1 [file2 ...]\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *fp;
+    for (int i = 1; i < argc; i++) {
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("unwzip: cannot open file\n");
+            return EXIT_FAILURE;
+        }
+        decode_print_RLE(fp);
+        fclose(fp);
+    }
     return EXIT_SUCCESS;
 }
