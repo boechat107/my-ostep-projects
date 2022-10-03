dd0362205e4827a061b8ec5180d0d51086ad8bec
Andre Ambrosio Boechat <boechat107@tutanota.com>
2022-10-03 16:14:01 -0300

    Add the basic implementation for "main"

--- initial-utilities/wzip-wunzip/wzip.c
+++ initial-utilities/wzip-wunzip/wzip.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[argc+1]) {
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        return EXIT_FAILURE;
+    }
 
+    FILE *fp;
+    for (int i = 1; i < argc; i++) {
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wzip: cannot open file\n");
+            return EXIT_FAILURE;
+        }
+        encode_print_RLE(fp);
+        fclose(fp);
+    }
     return EXIT_SUCCESS;
 }
