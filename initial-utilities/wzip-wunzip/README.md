5c398daa9a11be7e1080341788d8cae3be2015fb
Andre Ambrosio Boechat <boechat107@tutanota.com>
2022-10-03 18:52:19 -0300

    Add implementation for wzip
    commit 22901d36a6e5d593c9a61a7dc8a10bb1fbfeb613
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 17:32:02 2022 -0300
    
        Add code comments
    
    commit 90d4f255925c21b2ba6a058a3f83c74ac23bd74a
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 17:09:14 2022 -0300
    
        Make the function "to_bytes" more generic
    
    commit 9388ccbafb68b72b9ad8daeceafb8f7f1beee9f5
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 17:05:48 2022 -0300
    
        Fix the handler of the character "\0"; fix the use of fwrite
    
    commit cabf31c7d3d48a9c6a896848cca7d5eccc217852
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 16:53:59 2022 -0300
    
        Stop building unmodified source code
    
    commit ec087fbb67f0be844ac18a38570374fcad4e7393
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 16:15:13 2022 -0300
    
        Add functions to read file and convert number into bytes
    
    commit dd0362205e4827a061b8ec5180d0d51086ad8bec
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Mon Oct 3 16:14:01 2022 -0300
    
        Add the basic implementation for "main"
    
    commit aeb16b3aff7f7717e2b6d2c8cfd7661bd646489d
    Author: Andre Ambrosio Boechat <boechat107@tutanota.com>
    Date:   Tue Sep 27 23:37:57 2022 -0300
    
        Add the basic structure

---
+++ initial-utilities/wzip-wunzip/README.md
@@ -0,0 +1,12 @@
+# wgrep
+
+Mimics `grep`.
+
+## Usage
+
+```shell
+make
+./build/wcat match wcat.c
+# From STDIN.
+./build/wcat match < wcat.c
+```
---
+++ initial-utilities/wzip-wunzip/wzip.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Increase this number
+#define BUFFER_SIZE 10
+#define RLE_CNT_NBYTES 4
+
+/**
+   Converts the number "n" into an array of bytes.
+
+   "size" is the length of the given array.
+ **/
+void to_bytes(unsigned char *bytes, size_t size, unsigned long n) {
+    // In each iteration of the loop, we shift 1 byte to the right and we zero
+    // all bytes on the left ("255" makes only the least significant byte
+    // contain 1s).
+    //
+    // Example using 3 bytes (A, B and C):
+    //
+    // A - B - C
+    // 0:  0 - 0 - C  (n >> 0) & 255
+    // 1:  0 - 0 - B  (n >> 8) & 255
+    // 2:  0 - 0 - A  (n >> 16) & 255
+    for (size_t i = 0; i < size; i++) {
+        bytes[size - (i + 1)] = (n >> (8 * i)) & 255;
+    }
+}
+
+
+void encode_print_RLE(FILE *stream) {
+    // Reading buffer. We read files in parts.
+    char buffer[BUFFER_SIZE] = {0};
+    // The first RLE_CNT_NBYTES bytes to count repetitions. Used for printing.
+    unsigned char rle_cnt[RLE_CNT_NBYTES + 1] = {0};
+    // Used only to get the result of "fgets".
+    char *fgets_ret = buffer;
+    // The last character we counted. It changes when we find a new character.
+    char last_char = 0;
+    // The counter of each input character. It should represent the character
+    // in "last_char".
+    unsigned long char_cnt = 0;
+    // We read the file in chunks.
+    while((fgets_ret = fgets(buffer, BUFFER_SIZE, stream))) {
+        // For each chunk, we iterate on each character.
+        for (size_t i = 0; i < BUFFER_SIZE; i++) {
+            if (buffer[i] == last_char) {
+                char_cnt++;
+            } else if (buffer[i] == '\0') {
+                // We shouldn't count end of string characters.
+                break;
+            } else {
+                // We only need to check this because of the first character
+                // (worse for big files).
+                // An alternative would be initializing by reading a single
+                // character first (worse for small files).
+                if (char_cnt > 0) {
+                    // We build the 5-byte entries (4 for the counter, 1 for
+                    // the character).
+                    to_bytes(rle_cnt, RLE_CNT_NBYTES, char_cnt);
+                    rle_cnt[RLE_CNT_NBYTES] = last_char;
+                    // We write the entry to STDOUT in binary.
+                    fwrite(rle_cnt, 1, 5, stdout);
+                }
+                last_char = buffer[i];
+                char_cnt = 1;
+            }
+        }
+    }
+}
+
+
+int main(int argc, char *argv[argc+1]) {
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *fp;
+    for (int i = 1; i < argc; i++) {
+        fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wzip: cannot open file\n");
+            return EXIT_FAILURE;
+        }
+        encode_print_RLE(fp);
+        fclose(fp);
+    }
+    return EXIT_SUCCESS;
+}
